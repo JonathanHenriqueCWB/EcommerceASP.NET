8ddabf4344c591f23484b4ebc5148999ae382db9
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2019-10-10 22:03:40 -0300

    Cadastro de produto

---
+++ Ecommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.DAL;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoDAO _produtoDAO;
+        public ProdutoController(ProdutoDAO produtoDAO)
+        {
+            _produtoDAO = produtoDAO;
+        }
+
+        //Métodos dos controlers são chamados de actions
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(string txtNome, string txtDescricao, string txtQuantidade, string txtPreco)
+        {
+            Produto produto = new Produto();
+            produto.Nome = txtNome;
+            produto.Descricao = txtDescricao;
+            produto.Quantidade = Convert.ToInt32(txtQuantidade);
+            produto.Preco = Convert.ToDouble(txtPreco);
+
+            _produtoDAO.CadastrarProduto(produto);
+
+            return View();
+        }
+    }
+}
--- Ecommerce/Startup.cs
+++ Ecommerce/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ecommerce.DAL;
 using Ecommerce.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -32,8 +33,10 @@ namespace Ecommerce
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            //Configurnado Banco
+            //CONFIGURANDO BANCO
             services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("EcommerceConection")) );
+            //CONFIGURANDO A INJEÇÃO DE DEPENDÊNCIA
+            services.AddScoped<ProdutoDAO>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -57,7 +60,7 @@ namespace Ecommerce
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Produto}/{action=Cadastrar}/{id?}");
             });
         }
     }
