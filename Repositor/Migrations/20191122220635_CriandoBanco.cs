a1506e76e6d3375ff6dc863bd218f6d9bc410494
Jonathan Lima <jonathanhenrqiuecwb@gmail.com>
2019-11-22 19:10:23 -0300

    Instalando EntityFramewor no repository e add migration

---
+++ Repositor/Migrations/20191122220635_CriandoBanco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class CriandoBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    Descricao = table.Column<string>(maxLength: 100, nullable: false),
+                    Preco = table.Column<double>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false),
+                    CriadoEm = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
