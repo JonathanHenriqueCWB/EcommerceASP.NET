1340e9ea4774c7090b0001e44d922315d7640313
Jonanthan Lima <jonathanhenriquecwb@gmail.com>
2020-03-03 19:27:55 -0300

    Entidades pedido e itemPedido

---
+++ Domain/Models/ItemPedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class ItemPedido
+    {
+        [Key]
+        public int ItemPedidoId { get; set; }
+        public Produto Produto { get; set; }
+        public double Quantidade { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ Domain/Models/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+        public DateTime DataPedido { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public Usuario Usuario { get; set; }
+        public Endereco Endereco { get; set; }
+        public ICollection<ItemPedido> ItemPedidos { get; set; }
+    }
+}
